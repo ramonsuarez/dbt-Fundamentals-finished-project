8b5f12d191a7fe9434c972eb64f751b0b7d2e799
Ramon Suarez <ramon.suarez@laco.be>
2025-06-19 14:32:26 +0000

    Fix errors in original repo up to fact Still need to fix the dim

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,30 +1,37 @@
-with orders as  (
-    select * from {{ ref ('stg_jaffle_shop__orders' )}}
-),
+with
+    orders as (
+        select *
+        from {{ ref("stg_jaffle_shop__orders") }}
+    ),
 
-payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
-),
+    payments as (
+        select *
+        from {{ ref("stg_stripe__payments") }}
+    ),
 
-order_payments as (
-    select
-        order_id,
-        sum (case when status = 'success' then amount end) as amount
+    order_payments as (
+        select
+            order_id,
+            sum(case when status = 'success' then amount end) as amount
+        from payments
+        group by order_id
+    ),
 
-    from payments
-    group by 1
-),
+    customers as (
+        select *
+        from {{ ref("stg_jaffle_shop__customers") }}
+    ),
 
- final as (
+    final as (
+        select
+            orders.order_id,
+            customers.customer_id,
+            orders.order_date,
+            coalesce(order_payments.amount, 0) as amount
+        from orders
+        left join order_payments on orders.order_id = order_payments.order_id
+        left join customers on orders.customer_id = customers.customer_id
+    )
 
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
-
-    from orders
-    left join order_payments using (order_id)
-)
-
-select * from final
+select *
+from final
