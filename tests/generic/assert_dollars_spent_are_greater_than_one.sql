544cff2ce7c31b65c9df42a179775f2c6e8a8b08
Ramon Suarez <ramon.suarez@laco.be>
2025-06-24 11:32:07 +0000

    Add generic test

--- tests/generic/assert_dollars_spent_are_greater_than_one.sql
+++ tests/generic/assert_dollars_spent_are_greater_than_one.sql
@@ -1,12 +1,12 @@
 {% test average_dollars_spent_greater_than_one( model, column_name, group_by_column) %}
 
-select 
-    {{ group_by_column }},
-    avg( {{ column_name }} ) as average_amount
-
+{% set group_by_columns = group_by_column.split(',') | map('trim') | list %}
+select
+    {{ group_by_columns | join(',\n    ') }},
+    avg({{ column_name }}) as average_amount
 from {{ model }}
-group by {{ group_by_column }}
-having average_amount < 1
+group by {{ group_by_columns | join(', ') }}
+having avg({{ column_name }}) < 1
 
 
 {% endtest %}
