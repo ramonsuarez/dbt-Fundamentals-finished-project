f3912da7e4949e50dbde78e137c8e32d9d279950
Ramon Suarez <ramon.suarez@laco.be>
2025-07-23 14:23:44 +0000

    Clean up warnings, reorder projects file

--- tests/assert_amount_average_is_greater_than_one.sql
+++ tests/assert_amount_average_is_greater_than_one.sql
@@ -3,6 +3,6 @@
 select
     customer_id, 
     avg(amount) as average_amount
-from {{ ref('orders') }}
+from {{ ref('jaffle_shop', 'orders') }}
 group by customer_id
 having count(customer_id) > 1 and average_amount < 1
--- tests/assert_unique_customers_customer_id.sql
+++ tests/assert_unique_customers_customer_id.sql
@@ -1,5 +1,5 @@
 select
     customer_id, 
-from {{ ref('orders') }}
+from {{ ref('jaffle_shop', 'orders') }}
 group by customer_id
 having customer_id > 1
